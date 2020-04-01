42d30964586e461e3d6178e56b6e866c75764674
NADIR Hamza <hamzanadir.freelance@gmail.com>
2020-04-01 21:49:43 +0100

    reduce speed loading 1

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -23,13 +23,10 @@ The above copyright notice and this permission notice shall be included in all c
   <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
   <!--     Fonts and icons     -->
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
-  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
   <!-- CSS Files -->
-  <link href="{{ asset('theme/assets/css/material-dashboard.css?v=2.1.2') }}" rel="stylesheet" />
-  <!-- CSS Just for demo purpose, don't include it in your project -->
-  <link href="{{ asset('theme/assets/demo/demo.css') }}" rel="stylesheet" />
-  <link href="{{ asset('theme/assets/demo/demo.css') }}" rel="stylesheet" />
+  <link href="{{ asset('theme/assets/css/material-dashboard.min.css') }}" rel="stylesheet" />
+  <link href="{{ asset('theme/assets/css/datatables.min.css') }}" rel="stylesheet" />
+
   
 </head>
 
